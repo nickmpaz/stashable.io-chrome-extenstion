f42d53c2dd912a18a1f0e1a55d95696660307772
Nicholas Paz <nicholasmpaz@gmail.com>
2020-08-14 17:33:43 -0700

    hello world

---
+++ src/popup/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-card height="400" width="800" >
+      <v-btn color="success">text</v-btn>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+
+</style>
