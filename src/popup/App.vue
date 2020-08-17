d6256d1ef91afd9232bcf75ef9d53e65ecf1b36d
Nicholas Paz <nicholasmpaz@gmail.com>
2020-08-16 19:41:27 -0700

    save

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,25 +1,27 @@
 <template>
   <v-app>
-    <v-card height="400" width="800" >
+    <v-card height="400" width="800">
       <v-btn color="success">text</v-btn>
     </v-card>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
+  components: {},
+  created() {
+    var sending = browser.runtime.sendMessage({
+      message: "getApiToken",
+    });
+    sending.then(response => {
+      console.log(response.message)      
+    });
   },
-
   data: () => ({
     //
   }),
 };
 </script>
 
-<style lang="scss">
-
-</style>
